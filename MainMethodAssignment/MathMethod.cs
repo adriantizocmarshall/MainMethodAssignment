82e72a935002294e717fbbd989d2d9f80cc1204f
adriantizocmarshall <161913596+adriantizocmarshall@users.noreply.github.com>
2026-03-26 21:34:16 -0400

    Added comments to MathMethod for each method throughout class

--- MainMethodAssignment/MathMethod.cs
+++ MainMethodAssignment/MathMethod.cs
@@ -6,18 +6,21 @@ using System.Threading.Tasks;
 
 namespace MainMethodAssignment
 {
-    public class MathMethod
+    public class MathMethod // Class to overload a method three times using different data types
     {
+        // The beginning method multiplying an integer by four
         public int UserNumber(int userInput)
         {
             return userInput * 4;
         }
 
+        // This method takes a decimal as a parameter and returns an integer divided by two
         public int UserNumber(decimal userInput)
         {
             return (int)(userInput / 2);
         }
 
+        // This method takes a string as a parameter and returns an integer plus six after parsing the string
         public int UserNumber(string userInput)
         {
             int parsedString = int.TryParse(userInput, out int result) ? result : 0;
